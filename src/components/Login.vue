4bf5e65b4cf37f03af0b7bdecbb8691416f30bb4
ProjetInfoIlo <ilo38@live.fr>
2019-11-13 18:17:17 +0100

    Modifications Front (page de connexion)

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,40 @@
 <template>
-    <div></div>
+    <div>
+        <div id="div_titre">
+            <md-content class="md-title md-primary">
+                Connexion
+            </md-content>
+        </div>
+
+        <div id="div_form">
+            <md-content id="content_identifiant" class="md-title md-primary">
+                Identifiant
+            </md-content>
+
+
+            <md-field id="field_identifiant" class="md-theme-default">
+                <md-input id="input_identifiant" type="text" />
+            </md-field>
+
+            <md-content id="content_password" class="md-title md-primary">
+                Mot de passe
+            </md-content>
+
+            <md-field id="field_password">
+                <md-input id="input_password" type="password" class="md-primary" />
+            </md-field>
+        </div>
+
+        <div id="div_buttons">
+            <md-button id="button_valider">
+                <md-content class="md-primary">Se connecter</md-content>
+            </md-button>
+
+            <md-button id="button_creer" class="md-primary">
+                <md-content class="md-primary">Créer un compte</md-content>
+            </md-button>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +43,61 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    @import "../styles/global.scss";
 
+    #div_titre, #div_form, #button_valider, #button_creer {
+        border-radius: 15px;
+        border-style: none;
+        box-shadow: 3px 3px 3px gray;
+
+        background-color: $color-primary;
+
+        text-align: center;
+    }
+
+    #div_titre, #div_form {
+        margin-left: 25%;
+        margin-right: 25%;
+        padding-top: 1%;
+        padding-bottom: 1%;
+    }
+
+    #div_titre {
+        margin-top: 5%;
+    }
+
+    #content_identifiant, #content_password {
+        margin-top: 1%;
+    }
+
+    #div_form {
+        margin-top: 2%;
+    }
+
+    #field_identifiant, #field_password {
+        width: 30%;
+        margin: auto;
+    }
+
+    #input_identifiant, #input_password {
+        width: 100%;
+        text-align: center;
+    }
+
+    #div_buttons {
+        text-align: center;
+        margin-top: 5%;
+    }
+
+    #button_creer, #button_valider {
+        margin-left: 40%;
+        margin-right: 40%;
+
+        width: 18%;
+    }
+
+    #button_creer:hover, #button_valider:hover {
+        width: 20%;
+    }
 </style>
